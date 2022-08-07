2c09e6d5ff53f09bb48ec48a8964802012c17eaa
sebanimm <gks32114221@gmail.com>
2022-08-07 23:12:02 +0900

    2022-08-07 | 드롭다운 선택시 텍스트 변경

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const selected = document.querySelector(".selected");
 const optionsContainer = document.querySelector(".options-container");
 const description = document.getElementById("description");
 const optionsList = document.querySelectorAll(".option");
-const category = document.getElementsByName("category");
-const round = category.value;
 
 selected.addEventListener("click", () => {
   optionsContainer.classList.toggle("active");
@@ -11,9 +9,12 @@ selected.addEventListener("click", () => {
 
 optionsList.forEach((o) => {
   o.addEventListener("click", () => {
-    console.log(optionsList.value);
     selected.innerHTML = o.querySelector("label").innerHTML;
-    description.innerText = `64명의 후보 중 무작위 ${round}명이 대결합니다.`;
     optionsContainer.classList.remove("active");
   });
 });
+
+function getRound(category) {
+  const round = category.getAttribute("id");
+  description.innerText = `64명의 후보 중 무작위 ${round}명이 대결합니다.`;
+}
